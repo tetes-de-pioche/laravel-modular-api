e262bd15ac55a697b5137d6d6a0b10ad1ae2bd2e
agnonym <contact@agnonym.com>
2026-09-07 15:34:53 +0200

    * | Resource - Api: use ObfuscatedId / override toId() to handle ObfuscatedIds feature

--- src/Resources/ApiResource.php
+++ src/Resources/ApiResource.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace TetesDePioche\LaravelModularApi\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\JsonApi\JsonApiResource;
+use TetesDePioche\LaravelModularApi\Traits\Features\ObfuscatedId;
 
 class ApiResource extends JsonApiResource
 {
+    use ObfuscatedId;
+
+    public function toId(Request $request): string
+    {
+        $id = (string) $this->resource->getKey();
+
+        if (! $this->isObfuscatedIdsFeatureEnabled()) {
+            return $id;
+        }
+
+        return $this->encode($id);
+    }
 }
