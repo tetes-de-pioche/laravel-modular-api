b8e8d6e85b46db11ff5f110a8ff2028d280e63ad
agnonym <contact@agnonym.com>
2026-09-07 15:07:44 +0200

    * | Provider - LaravelModularApi: publishes, merges config

--- src/LaravelModularApiServiceProvider.php
+++ src/LaravelModularApiServiceProvider.php
@@ -8,7 +8,20 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelModularApiServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/modular-api.php',
+            'modular-api'
+        );
+    }
 
-    public function boot(): void {}
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/modular-api.php' => config_path('modular-api.php'),
+            ], 'modular-api-config');
+        }
+    }
 }
