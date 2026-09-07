2200db0ef938b34dd37e712d4afe604495d4d949
agnonym <contact@agnonym.com>
2026-09-07 15:04:08 +0200

    + | Provider - LaravelModularApi

---
+++ src/LaravelModularApiServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TetesDePioche\LaravelModularApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelModularApiServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void {}
+}
