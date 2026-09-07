8e30dc81ad1908c2a59da95d53a2bf1c2df6d9cb
agnonym <contact@agnonym.com>
2026-09-07 15:55:05 +0200

    * | Controller - Base: use ApiResponse

--- src/Http/Controllers/BaseController.php
+++ src/Http/Controllers/BaseController.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace TetesDePioche\LaravelModularApi\Http\Controllers;
 
+use TetesDePioche\LaravelModularApi\Traits\Http\ApiResponse;
+
 class BaseController
 {
+    use ApiResponse;
 }
