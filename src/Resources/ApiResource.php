2070d02aaf70c4f5a5753f7ada28e9e0de794d94
agnonym <contact@agnonym.com>
2026-09-07 15:33:11 +0200

    + | Resource - Api: extends JsonApiResource

---
+++ src/Resources/ApiResource.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TetesDePioche\LaravelModularApi\Resources;
+
+use Illuminate\Http\Resources\JsonApi\JsonApiResource;
+
+class ApiResource extends JsonApiResource
+{
+}
